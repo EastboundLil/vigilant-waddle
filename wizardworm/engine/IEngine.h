cd1db4597f562977b5dcdfa88953d56d67e23a85
Unknown <johnscape96@gmail.com>
2019-01-03 21:27:09 +0100

    gravity and epilepsy

--- wizardworm/engine/IEngine.h
+++ wizardworm/engine/IEngine.h
@@ -46,7 +46,8 @@ protected:
 	int engineType;
 
 	void StartThread();
-	void ColCheck(sf::Vector2f direction);
+	void ColCheck(sf::Vector2f direction, bool gravCheck = true);
+	void GravCheck();
 	void ReloadCollision();
 
 	enum EngineState
